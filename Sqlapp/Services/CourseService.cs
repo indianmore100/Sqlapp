16a7cf66b5b04137bfd7de573fe55b4cad4e62c0
Swapnil More <swapnil.more@nuance.com>
2021-12-30 14:33:16 +0530

    Add project files.

---
+++ Sqlapp/Services/CourseService.cs
@@ -0,0 +1,45 @@
+﻿using Sqlapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sqlapp.Services
+{
+    public class CourseService
+    {
+        private SqlConnection GetConnection(string _connectionstring)
+        {
+            return new SqlConnection(_connectionstring);
+        }
+
+        public IEnumerable<Course> GetCourses(string _connectionstring)
+        {
+            List<Course> _lst = new List<Course>();
+            string _statement = "SELECT CourseID,CourseName,rating from Course";
+            SqlConnection _connection = GetConnection(_connectionstring);
+            // Let's open the connection
+            _connection.Open();
+            // We then construct the statement of getting the data from the Course table
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+            // Using the SqlDataReader class , we will read all the data from the Course table
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Course _course = new Course()
+                    {
+                        CourseID = _reader.GetInt32(0),
+                        CourseName = _reader.GetString(1),
+                        Rating = _reader.GetDecimal(2)
+                    };
+
+                    _lst.Add(_course);
+                }
+            }
+            _connection.Close();
+            return _lst;
+        }
+    }
+}
