bbb54d0ec7719b36a22aa2740c3c8ef81053ddf1
Swapnil More <swapnil.more@nuance.com>
2022-01-10 16:21:19 +0530

    apconfiguaration

--- Sqlapp/Program.cs
+++ Sqlapp/Program.cs
@@ -19,8 +19,11 @@ namespace Sqlapp
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
+                webBuilder.ConfigureAppConfiguration(config =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration("Endpoint=https://appconfig0001.azconfig.io;Id=Nb32-l0-s0:k1pKUMrZZ8BnLZZ5YBcU;Secret=tGfUsAp9vEjDq+RKdO1bUTJUwPe0G1Fw3sF/Aug7eL0=");
+                })
+                   .UseStartup<Startup>());
     }
 }
